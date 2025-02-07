dc86f4b5f537bc69379d996627aaf7066a49ebe8
Lam Truong <truongnl032000@gmail.com>
2025-02-07 14:24:08 +0700

    Initial commit

--- src/app/(sub pages)/projects/page.js
+++ src/app/(sub pages)/projects/page.js
@@ -19,7 +19,7 @@ export default function Home() {
     <>
       <Image
         src={bg}
-        alt="Next.js Portfolio website's about page background image"
+        alt="My Portfolio website's about page background image"
         className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-50"
         priority
         sizes="100vw"
